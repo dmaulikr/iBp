d5a631e60c2848415dd0ae3b693b056eb694d5e1
nestcode <nestcode@nestcodes-mac-mini.local>
2018-01-02 14:38:28 +0530

    Initial Commit

---
+++ BrainPower/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BrainPower
+//
+//  Created by nestcode on 1/2/18.
+//  Copyright © 2018 nestcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BrainPower/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BrainPower
+//
+//  Created by nestcode on 1/2/18.
+//  Copyright © 2018 nestcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
